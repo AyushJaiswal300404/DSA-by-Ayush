31bb9e56a18965f8237d75a416b6bd4f18a2321a
AyushJaiswal300404 <ayushjaiswal300404@gmail.com>
2025-07-18 12:37:44 +0530

    graph

---
+++ Graph/graph.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<unordered_map>
+#include<list>
+using namespace std;
+
+class graph{
+    public:
+        unordered_map<int,list<int>>adj;
+        void addEdge(int u, int v, bool direction){
+            //direction=0 undirected
+            //direction=1 directed
+
+            //Create an edge from u to v
+            adj[u].push_back(v);
+            if(direction==0)    adj[v].push_back(u);
+        }
+        void printAdjList(){
+            for(auto it:adj){
+                cout<<it.first<<"->";
+                for(auto j: it.second){
+                    cout<<j<<",";
+                }
+                cout<<endl;
+            }
+        }
+};
+
+int main(){
+    int n;
+    cout<<"Enter the no. of nodes"<<endl;
+    cin>>n;
+
+    int m;
+    cout<<"Enter the no. of edges"<<endl;
+    cin>>m;
+
+    graph g;
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        g.addEdge(u,v,0);
+    }
+    g.printAdjList();
+    return 0;
+}
